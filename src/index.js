b68f7eec96effbd1194ae8fcf9c6d27041e74329
Lucas Lafèré <laferelucas@gmail.com>
2022-05-06 13:20:53 -0300

    componetizado

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import Corpo from "./components/Corpo";
+
+
+
+function App () {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.querySelector(".root"));
